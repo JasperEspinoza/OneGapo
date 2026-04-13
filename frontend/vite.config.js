195b5f97a44e41206a823ef6a15333a99d8c0810
JasperEspinoza <202310813@gordoncollege.edu.ph>
2026-04-14 01:19:00 +0800

    Deployment5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const isVercelPreview = process.env.VERCEL_ENV === 'preview';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      disable: isVercelPreview,
       registerType: 'prompt',
       includeAssets: ['pwa-icon.svg'],
       manifest: {
